e4de288d68cb3a2b2e733f293f28611774706f4e
shaojian <1303292392@qq.com>
2019-09-12 17:59:05 +0800

    update

--- opencv_tensorflow/Chapter 2/2.15 tf_matrix_2.py
+++ opencv_tensorflow/Chapter 2/2.15 tf_matrix_2.py
@@ -8,4 +8,26 @@
 # 矩阵加法： 矩阵的 shape 必须相同
 # 矩阵乘法： [A  B]   x   [E  F]   =  [AE+BG  AF+BH]
 #           [C  D]       [G  H]      [CE+DG  CF+DH]
+# 矩阵乘法要求： 第一个矩阵的列数和第二个矩阵的行数相同
+
+import tensorflow as tf
+data1 = tf.constant([[1, 2]])
+data2 = tf.constant([[3],
+					 [4]])
+data3 = tf.constant([[3, 3]])
+data4 = tf.constant([[1, 2],
+					 [3, 4],
+					 [5, 6]])
+
+"""矩阵加乘法运算"""
+mat_mul = tf.matmul(data1, data2)
+mat_mul2 = tf.multiply(data1, data2)
+mat_add = tf.add(data1, data3)
+with tf.Session() as sess:
+	print(sess.run(mat_mul))         # 矩阵乘法
+	print(sess.run(mat_mul2))        # 一般乘法
+	print(sess.run(mat_add))
+
+	# 列表封装多个计算
+	print(sess.run([mat_mul, mat_add]))
 
