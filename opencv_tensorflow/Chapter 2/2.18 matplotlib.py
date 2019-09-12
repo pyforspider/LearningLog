7fcf7dc1326244398fb99de469cb7e6fe0486878
shaojian <1303292392@qq.com>
2019-09-12 19:08:27 +0800

    update

--- opencv_tensorflow/Chapter 2/2.18 matplotlib.py
+++ opencv_tensorflow/Chapter 2/2.18 matplotlib.py
@@ -1,6 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+x = np.array(range(1, 9))
+y = np.array([3, 5, 7, 10, 2, 6, 9, 12])
 
+# 折线 plt.plot(x, y, color, lw=可选 ,)
+plt.plot(x, y, "r", lw=10)
+plt.show()
+
+# 折线 柱状 饼状
+# plt.plot(x, y, "r")
+# plt.bar(x, y, %lw, alpha=透明度[0, 1], color=颜色)
+plt.bar(x, y, 0.5, alpha=0.5, color="g")
+plt.show()
 
 
