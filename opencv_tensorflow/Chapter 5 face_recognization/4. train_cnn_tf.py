846a75ebf90816ae76862b23ccfdc8c38a2da9b6
shaojian <1303292392@qq.com>
2019-09-16 22:06:35 +0800

    update

--- opencv_tensorflow/Chapter 5 face_recognization/4. train_cnn_tf.py
+++ opencv_tensorflow/Chapter 5 face_recognization/4. train_cnn_tf.py
@@ -21,24 +21,25 @@ test_data = np.random.permutation(test_data)
 np.random.seed(100)
 test_label = np.random.permutation(test_label)
 
-# train [0-9] [10*N] [15*N]  [0 0 1 0 0 0 0 0 0 0] -> 2
+# train [0-9 标签] [10个数字*N] [15个人*N]  [0 0 1 0 0 0 0 0 0 0] -> 表示数字2
 # 准备训练数据,转换类型，对读入图片进行归一化处理，astype转换数据类型,/255是进行归一化，即像素0-1之间的转化
 # 转换完后，train_data数据集为三维，图片数量*64*64，其中64*64为每张图片矩阵的维度，1代表图片是黑白的 (165, 64, 64, 1)
 train_data = train_data.reshape(train_data.shape[0], 64, 64, 1).astype(np.float32) / 255  # .shape--> (165, 64, 64, 1)
-# 数据集共有15种人脸，所以建立165*15的数据标签
-train_labels_new = np.zeros((165, 15))  # 165 image 15
+# 数据集共有15个人，所以建立165*15的数据标签
+train_labels_new = np.zeros((165, 15))  # 165张图片 15个人 165张 x [0 0 1 0 0 0 0 0 0 0 0 0 0 0 0] 表示第3个人
 for i in range(0, 165):
-	j = int(train_label[i, 0]) - 1  # 1-15 0-14
-	train_labels_new[i, j] = 1
+	j = int(train_label[i, 0]) - 1  # 1-15 转化为 0-14
+	train_labels_new[i, j] = 1      # 知道了哪一个下标为1，将训练的数据标签从 0 更新至 1
+# 准备测试数据：
+test_data_input = test_data.reshape(test_data.shape[0], 64, 64, 1).astype(np.float32) / 255
+test_labels_input = np.zeros((64, 15))
+for i in range(0, 64):
+	j = int(test_label[i, 0]) - 1
+	test_labels_input[i, j] = 1
 
-# test_data_input = test_data.reshape(test_data.shape[0], 64, 64, 1).astype(np.float32) / 255
-# test_labels_input = np.zeros((64, 15))  # 165 image 15
-# for i in range(0, 64):
-# 	j = int(test_label[i, 0]) - 1  # 1-15 0-14
-# 	test_labels_input[i, j] = 1
-# # cnn acc  tf.nn tf.layer
-# data_input = tf.placeholder(tf.float32, [None, 64, 64, 1])
-# label_input = tf.placeholder(tf.float32, [None, 15])
+# cnn acc  tf.nn tf.layer
+data_input = tf.placeholder(tf.float32, [None, 64, 64, 1])
+label_input = tf.placeholder(tf.float32, [None, 15])
 #
 # layer1 = tf.layers.conv2d(inputs=data_input, filters=32, kernel_size=2, strides=1, padding='SAME',
 # 						  activation=tf.nn.relu)
